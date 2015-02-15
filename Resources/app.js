5d1675d9748693b1d8074e945ae2d893bc0de3f6
Douglas Arley <darley@Douglas-Arley-MBP.local>
2015-02-15 14:14:58 -0500

    Cleaned up some white space and comments

--- Resources/app.js
+++ Resources/app.js
@@ -1,42 +1,29 @@
 var api = require("api");
-// var geo = require("geo");
 
-
-// 
-
-// // geo.getGeo();
-// //ipad でやるとき↑
-if (Ti.Network.online ===true) {
+// iPad でやるとき↑
+if (Ti.Network.online === true) {
 	var Cloud = require("ti.cloud");
-Cloud.debug = true;
-
+	Cloud.debug = true;
 
 	console.log("online");
-	
-	
-	
-	//push to DB which is save
+		
+	// Push to DB which is save
 	Cloud.Users.login({
-		login:"nanap",
-		password:"12345"
-	},
-	function(e){
+		login : "nanap",
+		password : "12345"
+	}, function(e){
 		if(e.success){
 			api.bunbun();
-		}else{
+		} else {
 			alert("No Connection");
 		}
-		
 	});
-	// // //simulatorでやるとき↑
-// // 
+	// Simulator でやるとき↑
 
-} else if(Ti.Network.online === false) {
-
+} else if (Ti.Network.online === false) {
+	// Bring Data from DB favorite from final	
 	var data = require('data');
 	data.read();
+	
 	alert("No Connection");
-	// // //bring Data from DB favorite from final
-	// // alert("You are not connecting to any internet.");
-// // 	
 };
